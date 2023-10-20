88808dc92dd3ce8d936c3de0eec97e810787e5b1
Zangura <zanguraa@gmail.com>
2023-10-20 17:44:18 +0400

    upload solution

---
+++ Classroom.AgeBracketDetermination/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Classroom.AgeBracketDetermination
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while(true)
+            {
+
+                try
+                {
+                    Console.WriteLine("Please enter your age:");
+                    var age = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"You are in the {AgeBracket(age)} age bracket.");
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine($"Error: please enter integers");
+                }
+            }
+           
+           
+        }
+
+        static string AgeBracket(int age)
+        {
+            if (age < 0)
+            {
+                throw new ArgumentException("Age cannot be negative.");
+            }
+            else if (age < 3)
+            {
+                return "infant";
+            }
+            else if (age < 6)
+            {
+                return "toddler";
+            }
+            else if (age < 13)
+            {
+                return "child";
+            }
+            else if (age < 18)
+            {
+                return "teenager";
+            }
+            else if (age < 65)
+            {
+                return "adult";
+            }
+            else
+            {
+                return "senior";
+            }
+        }
+    }
+}
---
+++ Classroom.WeekdayOrWeekend/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Classroom.WeekdayOrWeekend
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var weekdays = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
+            var month = DateTime.Now.Month;
+
+
+
+            Console.WriteLine("Hello, please enter one of the day of weekend: ");
+            var day = Console.ReadLine();
+
+
+            Weekday(day);
+            MonthQuarter(month);
+
+        }
+
+        static void Weekday(string day)
+        {
+            switch (day.ToLower())
+            {
+                case "saturday":
+                case "sunday":
+                    Console.WriteLine("It is weekend");
+                    break;
+                case "monday":
+                case "muesday":
+                case "mednesday":
+                case "mhursday":
+                case "mriday":
+                    Console.WriteLine("It is weekday");
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid day");
+                    break;
+            }
+        }
+
+        static void MonthQuarter(int month)
+        {
+            switch (month)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    Console.WriteLine("Q1");
+                    break;
+                case 4:
+                case 5:
+                case 6:
+                    Console.WriteLine("Q2");
+                    break;
+                case 7:
+                case 8:
+                case 9:
+                    Console.WriteLine("Q3");
+                    break;
+                case 10:
+                case 11:
+                case 12:
+                    Console.WriteLine("Q4");
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid month");
+                    break;
+
+            }
+        }
+
+    }
+}

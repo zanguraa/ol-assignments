be4b94e338d335cbbce5a4c2b32d2e988a041259
Zangura <zanguraa@gmail.com>
2023-10-21 11:17:45 +0400

    created vowelornot project

---
+++ Assignments.VowelOrNot/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace Assignments.VowelOrNot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+            Console.WriteLine("Please Enter letter: ");
+            var userResponse = Console.ReadLine();
+            if (char.TryParse(userResponse, out letter))
+            {
+                Console.WriteLine(CheckVowelOrNot(letter));
+            }
+
+            
+        }
+
+        static bool CheckVowelOrNot(char letter)
+        {
+
+            int[] vowelChars = { 'a', 'e', 'i', 'o', 'u', 'ა', 'ე', 'ი', 'ო', 'უ' };
+           if (vowelChars.Contains(letter))
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+           
+        }
+    }
+}

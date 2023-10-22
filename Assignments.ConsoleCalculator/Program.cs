9a945ae8c8e8beafa8ae8b7173e0c8d82f7eaf14
Zangura <zanguraa@gmail.com>
2023-10-22 09:59:23 +0400

    added project basic, console output is parsed tu numbers etc..

--- Assignments.ConsoleCalculator/Program.cs
+++ Assignments.ConsoleCalculator/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            try
+            {
+                double first;
+                double second;
+
+                Console.WriteLine("Enter first number: ");
+                double firstNumber = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second number: ");
+                var secondNumber = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Match operation: + - * \\:");
+            }
+            catch 
+            {
+                Console.WriteLine("Text is not a number");
+            }
+
+           
+
         }
     }
 }

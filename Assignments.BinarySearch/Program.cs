de52e6622657bf3c052729191be83214aa7dec35
Zangura <zanguraa@gmail.com>
2023-10-23 23:27:29 +0400

    created project BinarySearch

---
+++ Assignments.BinarySearch/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignments.BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 0, 1, 5, 76, 234, 678 };
+            var target = 75;
+            Console.WriteLine("");
+            Console.WriteLine(BinarySearch(array, target));
+        }
+
+        static int BinarySearch(int[] array, int target)
+        {
+            if (!array.Contains(target))
+            {
+                return -1;
+            }
+            else
+            {
+                var index = Array.IndexOf(array, target);
+                return index;
+            }
+        }
+    }
+}

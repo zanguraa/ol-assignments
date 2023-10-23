a796e762fa602ca368236f57a0c5969f92ccc9eb
Zangura <zanguraa@gmail.com>
2023-10-23 12:12:03 +0400

    calculator project isfinished

--- Assignments.ConsoleCalculator/Program.cs
+++ Assignments.ConsoleCalculator/Program.cs
@@ -10,7 +10,7 @@
 
                 try
                 {
-                    
+
 
                     Console.WriteLine("Enter first number: ");
                     string firstNumber = Console.ReadLine();
@@ -19,25 +19,65 @@
                     string secondNumber = Console.ReadLine();
                     double parsedSecond = ParseToDouble(secondNumber);
                     Console.WriteLine("Enter Match operation: + - * \\:");
+                    string operation = Console.ReadLine();
+                    Calculate(parsedNumber, parsedSecond, operation);
+
+
                 }
                 catch
                 {
-                   
+                    Console.WriteLine("Text is not a number");
                 }
 
             }
 
         }
 
-         static double ParseToDouble(string input)
+        static double ParseToDouble(string input)
         {
             double result;
-            while(!double.TryParse(input, out result))
+            while (!double.TryParse(input, out result))
             {
                 Console.WriteLine("Text is not a number");
                 input = Console.ReadLine();
             }
             return result;
         }
+
+        static void Calculate(double first, double second, string op)
+        {
+            double result = 0;
+
+            if (op == "-")
+            {
+                result = first - second;
+                Console.WriteLine($" {first} - {second} = {result} ");
+
+            }
+            if (op == "+")
+            {
+                result = first + second;
+                Console.WriteLine($"{first} + {second} = {result}");
+            }
+            if (op == "*")
+            {
+                result = first * second;
+                Console.WriteLine($"{first} * {second} = {result}");
+            }
+            if (op == "/" && second != 0)
+            {
+                result = first / second;
+                Console.WriteLine($"{first} / {second} = {result}");
+            }
+            if (op == "/" && second == 0)
+            {
+                Console.WriteLine("Can not divide by zero");
+            }
+
+        }
+
+
     }
+
+
 }

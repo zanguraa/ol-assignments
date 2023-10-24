dd611236bc6a586af9b39973a4c5f062b8b2963c
Zangura <zanguraa@gmail.com>
2023-10-24 16:54:00 +0400

    add EvenNumberFinder project

---
+++ EvenNumberFinder/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EvenNumberFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, enter your number: ");
+            var parsedNum = int.Parse(Console.ReadLine());
+
+           GetEvenNumber(parsedNum);
+
+        }
+
+        static void GetEvenNumber(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("----");
+                    Console.WriteLine(i);
+                }
+            }
+            
+
+        }
+    }
+}

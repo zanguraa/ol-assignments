839ae26c766417e63522b549b0c746e6a09175c7
Zangura <zanguraa@gmail.com>
2023-10-24 16:42:55 +0400

    add numberprinter project

---
+++ Clasroom.NumberPrinter/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Clasroom.NumberPrinter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Hello, Enter your number: ");
+            var isParsed = int.TryParse(Console.ReadLine(), out num);
+            if(!isParsed)
+            {
+                Console.WriteLine("Error! please enter number");
+                return;
+            }
+            NumberPrinter(num);
+        }
+
+        static int NumberPrinter(int num)
+        {
+            for (int i = 0; i <= num; i++)
+            {
+                Console.WriteLine(i.ToString());
+            }
+            return num;
+        }
+    }
+}

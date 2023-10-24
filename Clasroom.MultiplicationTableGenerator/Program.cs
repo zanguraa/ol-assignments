975c16910d860cf4a2c24b2edc6fc9a8ecd52b9f
Zangura <zanguraa@gmail.com>
2023-10-24 17:21:47 +0400

    add MultiplicationTableGenerator

---
+++ Clasroom.MultiplicationTableGenerator/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Clasroom.MultiplicationTableGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter first number: ");
+                    var a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter second number: ");
+                    var b = double.Parse(Console.ReadLine());
+                    MultiplicationTableGenerator(a, b);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error, please enter valid number!");
+                }
+            }
+
+        }
+
+        static void MultiplicationTableGenerator(double a, double b)
+        {
+           for (int i = 1; i <= a; i++)
+            {
+                Console.Write(i + "\t");
+                for (int j = 1; j <= b; j++)
+                {
+                    Console.Write(i * j + "\t");
+                }
+                Console.WriteLine("\n");
+                }
+           
+
+        }
+    }
+}

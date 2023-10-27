294075987a8ec4c4698be1a5c67d8c82098ec586
Zangura <zanguraa@gmail.com>
2023-10-27 17:10:03 +0400

    add project DiscountCalculator

---
+++ Classroom.DiscountCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Classroom.DiscountCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, Engter your age: ");
+            var age = int.Parse(Console.ReadLine());
+            DiscountCalculator(age);
+        }
+
+        static void DiscountCalculator(int age)
+        {
+          
+            int price = 100;
+            if (age > 0)
+            {
+                var discount = (age < 18) ? price * 0.75 : (age >= 65) ? price * 0.85 : price;
+                Console.WriteLine($"your discounted price is {discount}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input please enter valid age");
+            }
+          
+           
+           
+        }
+    }
+}

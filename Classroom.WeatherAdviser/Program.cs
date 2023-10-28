73ce9476752c96e16d05db0a9730144a9a8c3f2c
Zangura <zanguraa@gmail.com>
2023-10-28 22:06:55 +0400

    added Enum for WeatherConditions

--- Classroom.WeatherAdviser/Program.cs
+++ Classroom.WeatherAdviser/Program.cs
@@ -2,9 +2,35 @@
 {
     internal class Program
     {
+
+       public enum WeatherConditions
+        {
+            Sunny,
+            Rainy,
+            Cloudy,
+            Windy,
+            Snowy
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter Weather condition here: ");
+            var userWeather = Console.ReadLine();
+            var parsedWeather = Enum.TryParse(userWeather, out WeatherConditions condition);
+
         }
+
+        public Dictionary<WeatherConditions, string> Conditions = new()
+        {
+            {WeatherConditions.Sunny, "Wear a t-shirt and sunglasses" },
+            {WeatherConditions.Rainy, "Take Umbrella and go for a walk" },
+            {WeatherConditions.Windy, "Stay at home, Avoid going to Didi Dighomi" },
+            {WeatherConditions.Snowy, "Go to Bakuriani, or Gudauri" },
+            {WeatherConditions.Cloudy, "Take Camera ond go to Photosession" }
+
+
+        };
+
+
     }
 }

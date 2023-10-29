daa779d9a5d398dfe69b8ef03508e18a02f7cd45
Zangura <zanguraa@gmail.com>
2023-10-29 12:20:12 +0400

    added project MoveElementToEnd

---
+++ Assignments.MoveElementToEnd/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Assignments.MoveElementToEnd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new List<int> { 2, 1, 2, 2, 2, 3, 4, 2 };
+            var toMove = 2;
+            MoveElementToEnd(toMove, array);
+
+            foreach (var element in array)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        static void MoveElementToEnd(int toMove, List<int> array)
+        {
+
+            // Implement logic to move element to the end of the list
+            int indexElement = array.RemoveAll(x => x == toMove);
+            // Find the index of the element
+           for (int i = 0; i < indexElement; i++)
+            {
+                array.Add(toMove);
+            }
+            
+        }
+    }
+}

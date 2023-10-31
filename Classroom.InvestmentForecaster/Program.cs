a88a93e8ac4c5b7ca73c543fb5ed47ccc51eb96f
Zangura <zanguraa@gmail.com>
2023-10-31 17:30:27 +0400

    add InvestmentForecaster project

---
+++ Classroom.InvestmentForecaster/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Classroom.InvestmentForecaster
+{
+    internal class Program
+    {
+       static double principalAmount = 0;
+       static double annualAmount = 0;
+       static double timeInYears = 0;
+       static readonly double n = 12;
+        static void Main(string[] args)
+        {
+             
+
+             ParseInputs();
+            CalculateFutureValue();
+        }
+
+        public static void ParseInputs()
+        {
+            Console.WriteLine("Hello, Please enter your principal amount: ");
+            principalAmount = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter annual interest rate: ");
+            annualAmount = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter number of year: ");
+            timeInYears = double.Parse(Console.ReadLine());
+        }
+
+        public static void CalculateFutureValue()
+        {
+            var rate = annualAmount / 100;
+            var futureValue = principalAmount * Math.Pow(1 + rate /n, (n * timeInYears));
+
+
+            Console.WriteLine($"Future value of your investment: ${futureValue}");
+        }
+
+    }
+}

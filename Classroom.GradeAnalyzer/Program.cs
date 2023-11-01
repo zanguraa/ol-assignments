fac0e049734d294e0a7d50152949e56783033ff3
Zangura <zanguraa@gmail.com>
2023-11-01 19:48:37 +0400

    upload changes

---
+++ Classroom.GradeAnalyzer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Classroom.GradeAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

91798aa53684c82f0e66afa535cee600775d2a66
Zangura <zanguraa@gmail.com>
2023-11-02 01:36:59 +0400

    added project GradeAnalyzer basics

--- Classroom.GradeAnalyzer/Program.cs
+++ Classroom.GradeAnalyzer/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your grades separated by commas: ");
+            var userGrades = Console.ReadLine();
+            var gradesArray = userGrades.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+
+            var gradeSum = 0;
+
+            foreach ( var gradeString in gradesArray )
+            {
+                var parsedGrade = int.TryParse(gradeString, out var grade );
+
+                gradeSum += grade;
+            }
+           
+            var gradeAvvarage = gradeSum / gradesArray.Length;
+
+            Console.WriteLine(gradeAvvarage.ToString());
+           
         }
     }
 }

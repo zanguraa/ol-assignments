8b58a418f536be54b51ccff8d49c9df584cac7d4
Zangura <zanguraa@gmail.com>
2023-11-02 12:31:06 +0400

    re-edited logic.

--- Assignments.BinarySearch/Program.cs
+++ Assignments.BinarySearch/Program.cs
@@ -5,22 +5,42 @@
         static void Main(string[] args)
         {
             var array = new int[] { 0, 1, 5, 76, 234, 678 };
-            var target = 75;
+            var target = 76;
             Console.WriteLine("");
-            Console.WriteLine(BinarySearch(array, target));
-        }
-
-        static int BinarySearch(int[] array, int target)
-        {
-            if (!array.Contains(target))
+           int result = BinarySearch(array, target);
+            if (result != -1)
             {
-                return -1;
+                Console.WriteLine($"Element {target} found at index {result}.");
             }
             else
             {
-                var index = Array.IndexOf(array, target);
-                return index;
+                Console.WriteLine($"{target} not found in the array!");
+
             }
+        }
+
+       public static int BinarySearch(int[] array, int target)
+        {
+           int left = 0;
+            int right = array.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (array[mid] == target)
+                {
+                    return mid;
+                }
+                else if (array[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return -1;
         }
     }
 }

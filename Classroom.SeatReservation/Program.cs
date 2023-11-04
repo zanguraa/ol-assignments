4faa376a5c6f983e8448203bd154ffd0bd3fed61
Zangura <zanguraa@gmail.com>
2023-11-05 00:57:02 +0400

    added reserveseats function

--- Classroom.SeatReservation/Program.cs
+++ Classroom.SeatReservation/Program.cs
@@ -9,24 +9,45 @@ namespace Classroom.SeatReservation
         static int[,] seatArray = new int[5, 3];
         static void Main(string[] args)
         {
-            Console.WriteLine("Select an option:\r\n");
-           
-            var userChoice = Console.ReadLine();
-            var choosedSeats = userChoice.Split(",", StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine();
+
             DisplaySeats(seatArray);
-            
+            var operation = AnsiConsole.Prompt(
+                 new SelectionPrompt<string>()
+                     .Title("Select an option:\r\n")
+                     .PageSize(3)
+                     .HighlightStyle("red")
+                     .AddChoices(new[] {
+            "1) Reserve a seat", "2) View available seats"
+                     }));
+            Console.WriteLine($"Your choice: {operation}");
+            ReserveSeats(seatArray);
+            DisplaySeats(seatArray);
         }
         static void DisplaySeats(int[,] seatArray)
         {
+           
             Console.WriteLine("Initial Seating Chart:\r\n");
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    Console.Write(seatArray[i, j] + "");
+                    Console.Write(" " + seatArray[i, j] + " ");
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine();
         }
+        static void ReserveSeats(int[,] seatArray)
+        {
+            Console.WriteLine("Enter the row number: ");
+            var num1 = int.TryParse(Console.ReadLine(), out int ParsedNum1);
+            Console.WriteLine("Enter the Column number: ");
+            var num2 = int.TryParse(Console.ReadLine(),out int ParsedNum2);
+
+           seatArray[ParsedNum1, ParsedNum2] = 1;
+           
+        }
+        
     }
 }

6f49599a1932404aa45085cc15f782aa393f8957
Zangura <zanguraa@gmail.com>
2023-11-04 23:12:28 +0400

    added project SeatReservation and in this project is ready only display seats function

---
+++ Classroom.SeatReservation/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Classroom.SeatReservation
+{
+    internal class Program
+    {
+        static int[,] seatArray = new int[5, 3];
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            DisplaySeats(seatArray);
+        }
+
+        static void DisplaySeats(int[,] seatArray)
+        {
+            Console.WriteLine("Seat Reservation System\n");
+            for (int i = 0; i < 5; i++)
+            {
+                
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(seatArray[i, j] + "");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

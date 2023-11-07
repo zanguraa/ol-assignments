f138daabe68be514b0ab67330b10f74d3273b052
Zangura <zanguraa@gmail.com>
2023-11-07 17:54:25 +0400

    added Textanalyzer project with two function CountWords and FindMostCommonWord

---
+++ Classroom.TextAnalyzer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Classroom.TextAnalyzer
+{
+    
+
+    internal class Program
+    {
+        private static Dictionary<string, int> wordsDictionary = new();
+        static void Main(string[] args)
+        {
+            var pattern = @"[.,:#*^&<>\\]";
+            Console.WriteLine("Please enter a block of text for analysis:\r\n");
+            var userInput = Console.ReadLine();
+            var words = Regex
+                                    .Replace(userInput, pattern, " ")
+                                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            CountWords(words);
+            FindMostCommonWord(words);
+        }
+        static void CountWords(string[] words)
+        {
+            var count = 0;
+            foreach (var word in words)
+            {
+                count++;
+            }
+            Console.WriteLine($"Word Count: {count}");
+        }
+        static void FindMostCommonWord(string[] words)
+        {
+            foreach (var word in words)
+            {
+                if(!wordsDictionary.ContainsKey(word))
+                {
+                    wordsDictionary.Add(word, 1);
+                }
+                else
+                {
+                    wordsDictionary[word]++;
+                }
+            }
+            Console.WriteLine($"Most common word: {wordsDictionary.OrderByDescending(x => x.Value).First().Key}");
+        }
+        static void FindLongestWord(string[] words)
+        {
+
+
+        }
+    }
+}

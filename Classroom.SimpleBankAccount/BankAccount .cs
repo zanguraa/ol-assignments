a66418fc05b4ca9cb39a22f53cd96763c48192a2
Zangura <zanguraa@gmail.com>
2023-11-08 18:08:21 +0400

    added bankaccount project

---
+++ Classroom.SimpleBankAccount/BankAccount .cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classroom.SimpleBankAccount
+{
+    internal class BankAccount
+    {
+      public decimal Balance { get; private set; }
+      
+        public BankAccount(decimal balance)
+        { 
+            if (balance < 0)
+            {
+                throw new ArgumentException("Can not be less then zero");
+            }
+
+            this.Balance = balance; 
+           
+        }
+
+        public void Deposit(decimal amount)
+        {
+           if(amount < 0)
+            { throw new ArgumentException("Can not be less then zero"); }
+            Balance += amount;
+            Console.WriteLine($"Depositing: {amount}");
+        }
+
+
+    }
+}
---
+++ Classroom.SimpleBankAccount/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Classroom.SimpleBankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var account = new BankAccount(2000);
+            account.Deposit(767);
+
+        }
+
+    }
+}

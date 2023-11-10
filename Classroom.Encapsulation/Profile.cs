ef637b1619e6f6bde255bb68153c727d98111e27
Zangura <zanguraa@gmail.com>
2023-11-10 22:37:13 +0400

    finished project Encapsulation

---
+++ Classroom.Encapsulation/Profile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classroom.Encapsulation
+{
+    internal class Profile
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Profile(string name, int age)
+        {
+            SetProfile(name, age);
+            UpdateProfile(name, age);
+        }
+        private void SetProfile(string name, int age)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Please enter a valid name");
+                }
+                if (age <= 0)
+                {
+                    Console.WriteLine("Age must be greater than 0");
+                }
+                Name = name;
+                Age = age;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+        public void UpdateProfile(string updatedName, int updatedAge)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(updatedName))
+                {
+                    throw new ArgumentNullException(nameof(updatedName), "Please enter a valid name");
+                }
+                if (updatedAge <= 0)
+                {
+                    throw new ArgumentException("Age must be greater than 0", nameof(updatedAge));
+                }
+                Console.WriteLine("Updating profile...\r\n");
+                Name = updatedName;
+                Console.WriteLine($"Setting name to '{Name}'. Name updated successfully.\r\n");
+
+                Age = updatedAge;
+                Console.WriteLine($"Setting age to {Age}. Age updated successfully.\r\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Classroom.Encapsulation/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Classroom.Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var profile = new Profile("Valeri", -32);
+            var person2 = new Profile("Barbara", 19);
+        }
+    }
+}

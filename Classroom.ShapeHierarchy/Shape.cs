95a7c084d74c4d343ec4410e87ee1c755d035436
Zangura <zanguraa@gmail.com>
2023-11-16 21:13:09 +0400

    added subCLasses Circle and Rectangle with methods wich was ovveride from parent class Shape

--- Classroom.ShapeHierarchy/Shape.cs
+++ Classroom.ShapeHierarchy/Shape.cs
@@ -6,23 +6,43 @@ using System.Threading.Tasks;
 
 namespace Classroom.ShapeHierarchy
 {
-    abstract class Shape
+    public abstract class Shape
     {
         public abstract double CalculateArea();
         public abstract double CalculatePerimeter();
     }
-    public class Circle : Shape 
-    { 
-       public double Radius { get; set; }
-        public Circle(double radius) 
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
         {
             Radius = radius;
         }
-
         public override double CalculateArea()
         {
-            
             return Math.PI * Math.Pow(Radius, 2);
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * Radius * Math.PI;
+        }
+    }
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Length { get; set; }
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+        public override double CalculateArea()
+        {
+            return Width * Length;
+        }
+        public override double CalculatePerimeter()
+        {
+            return (Length + Width) * 2;
         }
     }
 }

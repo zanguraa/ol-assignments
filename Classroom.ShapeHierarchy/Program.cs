3ea5505c007419a79998954d9ac7ab5746ba5a1a
Zangura <zanguraa@gmail.com>
2023-11-16 22:22:08 +0400

    finished Shape project

--- Classroom.ShapeHierarchy/Program.cs
+++ Classroom.ShapeHierarchy/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace Classroom.ShapeHierarchy
+﻿using System.Reflection.Metadata;
+
+namespace Classroom.ShapeHierarchy
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Circle circle = new Circle(5);
+           var perimeterCircle = circle.CalculatePerimeter();
+           var areaCircle = circle.CalculateArea();
+            Rectangle rec = new Rectangle(4, 3);
+            var rectPerimeter = rec.CalculatePerimeter();
+            var rectArea = rec.CalculateArea();
+            Triangle triangle = new Triangle(3, 4, 5);
+            var trianglePerimeter = triangle.CalculatePerimeter();
+            var triangleArea = triangle.CalculateArea();
+
+            CalculateCircle(circle, areaCircle, perimeterCircle);
+            Console.WriteLine();
+            CalculateRectangle(rec, rectPerimeter, rectArea);
+            Console.WriteLine();
+            CalculateTriangle(triangle, triangleArea, trianglePerimeter);
+        }
+
+        static void CalculateCircle(Circle circle, double area, double perimeter)
+        {
+            Console.WriteLine($"Calculate with Radius: {circle.Radius}");
+            Console.WriteLine($"Area: {area.ToString("0.00")}");
+            Console.WriteLine($"Perimeter {perimeter.ToString("0.00")}");
+        }
+        static void CalculateRectangle(Rectangle rec, double perimeter, double area)
+        {
+            Console.WriteLine($"Rectangle with length {rec.Length} and width {rec.Width}");
+            Console.WriteLine($"Area: {area}");
+            Console.WriteLine($"Perimeter {perimeter}");
+        }
+        static void CalculateTriangle(Triangle triangle, double perimeter, double area)
+        {
+            Console.WriteLine($"Triangle widh sides {triangle.Side1}, {triangle.Side2}, {triangle.Side3} ");
+            Console.WriteLine($"Area: {area}");
+            Console.WriteLine($"Perimeter: {perimeter}");
         }
     }
 }
--- Classroom.ShapeHierarchy/Shape.cs
+++ Classroom.ShapeHierarchy/Shape.cs
@@ -45,4 +45,25 @@ namespace Classroom.ShapeHierarchy
             return (Length + Width) * 2;
         }
     }
+    public class Triangle : Shape
+    {
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+        public double Side3 { get; set; }
+        public Triangle(double side1, double side2, double side3)
+        {
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+        }
+        public override double CalculatePerimeter()
+        {
+            return Side1 + Side2 + Side3;
+        }
+        public override double CalculateArea()
+        {
+            double s = (Side1 + Side2 + Side3) / 2;
+            return Math.Sqrt(s * (s - Side1) * (s - Side2) * (s - Side3));
+        }
+    }
 }

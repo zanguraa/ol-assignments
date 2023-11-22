ff4ce6b3d7fe211d3f146fdb96aba1fcaabb0f80
Zangura <zanguraa@gmail.com>
2023-11-22 20:10:06 +0400

    finished project textanalyzer

--- Classroom.TextAnalyzerMedium/Program.cs
+++ Classroom.TextAnalyzerMedium/Program.cs
@@ -1,36 +1,60 @@
-﻿namespace Classroom.TextAnalyzerMedium
+﻿using System;
+using System.IO;
+
+namespace Classroom.TextAnalyzerMedium
 {
     internal class Program
     {
+        static int totalWords = 0;
+        static string longestWord = "";
         static void Main(string[] args)
         {
             var file = @"C:\Users\zangura\Downloads\test.txt";
             Console.WriteLine("Please enter the file Path: ");
             var filePath = Console.ReadLine();
 
-            if(!File.Exists(filePath))
+            if (!File.Exists(filePath))
             {
-               throw new FileNotFoundException("file path is not correct");
+                throw new FileNotFoundException("File path is not correct");
             }
+            var content = File.ReadAllLines(filePath);            
 
-            var content1 = File.ReadAllText(filePath);
+            foreach (var line in content)
+            {
+                int wordsInLine = CountWords(line);
+                totalWords += wordsInLine;
 
-            Console.WriteLine(content1);
-
-            //using (StreamReader reader = new StreamReader(filePath))
-            //{
-
-            //    string content = reader.ReadToEnd();
-
-            //    Console.WriteLine("File content:\n" + content);
-            //}
-            CountWords(content1);
+                string longestWordInLine = FindLongestWord(line);
+                if (longestWordInLine.Length > longestWord.Length)
+                {
+                    longestWord = longestWordInLine;
+                }
+            }
+            Console.WriteLine($"Total words: {totalWords}");
+            Console.WriteLine($"Longest word across all lines is: {longestWord}");
         }
 
-        static void CountWords(string word)
+        static int CountWords(string line)
         {
-            string[] filteredWords = word.Split(new[] { ' ', '.' }, StringSplitOptions.RemoveEmptyEntries);
-            Console.WriteLine($" words: {filteredWords.Length} ");
+            string[] words = line.Split(new[] { ' ', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+
+        static string FindLongestWord(string line)
+        {
+            string[] words = line.Split(new[] { ' ', '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string longestWordInLine = words.Length > 0 ? words[0] : "";
+
+            foreach (var word in words)
+            {
+                if (word.Length > longestWordInLine.Length)
+                {
+                    longestWordInLine = word;
+                }
+            }
+
+            return longestWordInLine;
         }
     }
 }

a493999a59a90e46dc6295970fea5249f447ace9
Zangura <zanguraa@gmail.com>
2023-11-26 13:59:27 +0400

    added updated task method and print task method also

--- Classroom.TaskTracker/TaskTracker.cs
+++ Classroom.TaskTracker/TaskTracker.cs
@@ -15,5 +15,24 @@ namespace Classroom.TaskTracker
             Task newTask = new Task { TaskName = taskName, TaskStatus = taskStatus };
             tasks.Add(taskName, newTask);
         }
+
+        public void UpdateTask(string taskName, string taskStatus)
+        {
+            if (tasks.ContainsKey(taskName))
+            {
+                tasks[taskName].TaskStatus = taskStatus;
+            }
+            else
+            {
+                Console.WriteLine(" Console.WriteLine($\"Task '{taskName}' not found.\");");
+            }
+        }
+        public void PrintTasks()
+        {
+            foreach (var task in tasks)
+            {
+                Console.WriteLine($"Task: {task.Key}, Status: {task.Value.TaskStatus}");
+            }
+        }
     }
 }

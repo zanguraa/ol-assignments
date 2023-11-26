b784e4fbd5fd3e970e54a5bf93d1adcc585f8c9d
Zangura <zanguraa@gmail.com>
2023-11-26 13:22:58 +0400

    added tasktracker class with addtask method

---
+++ Classroom.TaskTracker/TaskTracker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classroom.TaskTracker
+{
+    internal class TaskTracker
+    {
+        public Dictionary<string, Task> tasks = new Dictionary<string, Task>();
+
+        public void AddTask(string taskName, string taskStatus)
+        {
+            Task newTask = new Task { TaskName = taskName, TaskStatus = taskStatus };
+            tasks.Add(taskName, newTask);
+        }
+    }
+}

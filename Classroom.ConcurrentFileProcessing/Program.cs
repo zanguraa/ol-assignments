68b0090aa38924f8d17eeb38c74287528154f7d7
Zangura <zanguraa@gmail.com>
2023-12-02 23:21:12 +0400

    added user consolereadlines

--- Classroom.ConcurrentFileProcessing/Program.cs
+++ Classroom.ConcurrentFileProcessing/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the operation (countlines/findword): countlines\r\n");
+            Console.WriteLine("Please enter the operation (countlines/findword): \r\n");
+            var operation = Console.ReadLine();
+            Console.WriteLine("Enter file paths (separated by a comma): ");
+            var chooseFile = Console.ReadLine();
+
         }
     }
 }

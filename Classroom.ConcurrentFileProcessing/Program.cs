0db12213973b6ec4e23d0132b7a40549d5087ec5
Zangura <zanguraa@gmail.com>
2023-12-02 23:19:31 +0400

    added welcome text in console

--- Classroom.ConcurrentFileProcessing/Program.cs
+++ Classroom.ConcurrentFileProcessing/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter the operation (countlines/findword): countlines\r\n");
         }
     }
 }

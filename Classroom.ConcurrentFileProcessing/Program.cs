58e744ef86ec80c870f6abd38747691d83dbe330
Zangura <zanguraa@gmail.com>
2023-12-03 02:13:09 +0400

    added CountLines Method

--- Classroom.ConcurrentFileProcessing/Program.cs
+++ Classroom.ConcurrentFileProcessing/Program.cs
@@ -2,13 +2,42 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            var file1 = @"C:\Users\zangura\Downloads\text.txt";
+            var file2 = @"C:\Users\zangura\Downloads\text2.txt";
+
             Console.WriteLine("Please enter the operation (countlines/findword): \r\n");
             var operation = Console.ReadLine();
             Console.WriteLine("Enter file paths (separated by a comma): ");
-            var chooseFile = Console.ReadLine();
+            var filePath = Console.ReadLine();
+            ChooseOperation(filePath, operation);
 
         }
+        static void ChooseOperation(string operation, string filePath)
+        {
+            if (operation.ToLower() == "countlines")
+            {
+            CountLines(filePath);
+            }
+            else if(operation.ToLower() == "findword")
+            {
+                Console.WriteLine("enter the word to find: ");
+                var wordToFind = Console.ReadLine();
+            }
+        }
+        static void CountLines(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            int linesCount = lines.Length;
+            Console.WriteLine($"{filePath} - Lines Counted: {linesCount}");
+        }
+        static void FindWord(string filePath, string wordToFind)
+        {
+            string words = File.ReadAllText(filePath);
+            
+        }
     }
+
 }

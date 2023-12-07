ad47f027b78ff6a1b3ff1fec384c74b1fc622be5
Zangura <zanguraa@gmail.com>
2023-12-08 00:38:08 +0400

    modified program.cs file for instances of dependencies

--- Classroom.RefactoringforSingleResponsibilityPrinciple/Program.cs
+++ Classroom.RefactoringforSingleResponsibilityPrinciple/Program.cs
@@ -1,16 +1,14 @@
 ﻿using Classroom.RefactoringforSingleResponsibilityPrinciple;
 
-public class BookStore
+class Program
 {
-   
+    static void Main(string[] args)
+    {
+        var bookRepository = new BookRepository();
+        var inventorymanager = new InventoryManager();
+        var emailService = new EmailService();
 
-   
-
-   
-
-  
-
-   
-
-    
+        var saleProcessor = new SaleProcessor(bookRepository, inventorymanager, emailService);
+        saleProcessor.ProcessPayment(userId: 123, bookId: 456);
+    }
 }

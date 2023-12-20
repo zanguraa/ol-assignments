c8b17123961775eacace806a103b0f1aa8758c0b
Zangura <zanguraa@gmail.com>
2023-12-21 00:24:24 +0400

    added two extension fucntions IsNumber and IsDate

--- Assignments.ExtensionFunctions/Program.cs
+++ Assignments.ExtensionFunctions/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var number = "01.2020";
+            number.IsNumber();
+            Console.WriteLine($"Is number or not: {number.IsNumber()} ");
+            Console.WriteLine($"Is Date or not: {number.IsDate()}");
+
         }
     }
 }
---
+++ Assignments.ExtensionFunctions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments.ExtensionFunctions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNumber(this string str)
+        {
+            return double.TryParse(str, out  var number);   
+        }
+
+        public static bool IsDate(this string str)
+        {
+            return DateTime.TryParse(str, out var date);
+        }
+    }
+}

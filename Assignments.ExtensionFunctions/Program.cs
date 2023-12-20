fdf6a567cf5a019050b6116114094a23e3451b06
Zangura <zanguraa@gmail.com>
2023-12-20 23:45:19 +0400

    added a blank project about Extension Functions

---
+++ Assignments.ExtensionFunctions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignments.ExtensionFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

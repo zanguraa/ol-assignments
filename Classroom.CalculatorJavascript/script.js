e37dc6c04c7b951a4748b5667aa1f12d4f8eeffa
Zangura <zanguraa@gmail.com>
2024-01-08 22:23:25 +0400

    added JavascriptCalculator project

---
+++ Classroom.CalculatorJavascript/script.js
@@ -0,0 +1,9 @@
+let plus = document.querySelector("#plus");
+let minus = document.querySelector("#minus");
+let divideOperator = document.querySelector("#divide");
+let multiplyOperator = document.querySelector("#multiply");
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;

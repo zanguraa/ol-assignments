b3c85c7778ae7282e472c7293e5879bdd3b5c9e8
Zangura <zanguraa@gmail.com>
2024-01-09 15:32:20 +0400

    added functions in js calculator

--- Classroom.CalculatorJavascript/script.js
+++ Classroom.CalculatorJavascript/script.js
@@ -1,9 +1,55 @@
-let plus = document.querySelector("#plus");
-let minus = document.querySelector("#minus");
-let divideOperator = document.querySelector("#divide");
-let multiplyOperator = document.querySelector("#multiply");
+let numbers = document.querySelectorAll(".numbers");
+let operators = document.querySelectorAll(".operators");
+let display = document.querySelector(".display-input");
+let reset = document.querySelector("#clear");
+let result = document.querySelector("#equal");
 
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    display.value += number.innerHTML;
+  });
+});
+
+const addition = (a, b, op) => {
+  return `${a} ${op} ${b}`;
+};
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    display.value += operator.innerHTML;
+  });
+});
+
+reset.addEventListener("click", () => {
+  display.value = "";
+});
+
+result.addEventListener("click", () => {
+ 
+    let input = display.value;
+    let regex = /(\d+)(\+|-|\*|\/)(\d+)/;
+    let match = input.match(regex);
+    let a = parseInt(match[1]);
+    let b = parseInt(match[3]);
+    let op = match[2];
+    let result = 0;
+    switch (op) {
+        case "+":
+        result = a + b;
+        break;
+        case "-":
+        result = a - b;
+        break;
+        case "*":
+        result = a * b;
+        break;
+        case "/":
+        if (b == 0) {
+            result = "Error";
+        } else {
+            result = a / b;
+        }
+        break;
+    }
+    display.value = result;
+});

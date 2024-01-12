0c22b4d4218de5e5993a3befc6642a10bbc7fc02
Zangura <zanguraa@gmail.com>
2024-01-13 00:04:57 +0400

    added Javascript quizz app with startup sources

---
+++ Classroom.SimpleJavascriptQuiz/script.js
@@ -0,0 +1,66 @@
+let questionEl = document.querySelector(".question");
+let choices = document.getElementById("quiz").querySelector(".answers");
+let currentQuestion = 0;
+let score = 0;
+
+let questions = [
+  {
+    question: "Which country is the last world cup champion in football?",
+    choices: ["France", "Brazil", "Germany", "Argentina"],
+    correctAnswer: "Argentina",
+  },
+  {
+    question: "Deepest lake in the world?",
+    choices: [
+      "Lake Baikal",
+      "Lake Superior",
+      "Lake Victoria",
+      "Lake Tanganyika",
+    ],
+    correctAnswer: "Lake Baikal",
+  },
+  {
+    question: "What is the capital of Australia?",
+    choices: ["Sydney", "Melbourne", "Canberra", "Perth"],
+    correctAnswer: "Canberra",
+  },
+  {
+    question: "What is the capital of Canada?",
+    choices: ["Toronto", "Vancouver", "Ottawa", "Montreal"],
+    correctAnswer: "Ottawa",
+  },
+  {
+    question: "Which country is 1st in the world in population?",
+    choices: ["China", "India", "USA", "Indonesia"],
+    correctAnswer: "China",
+  },
+];
+
+// Function to update the question and choices in the HTML
+function updateQuestion() {
+  questionEl.textContent = questions[currentQuestion].question;
+  choices.innerHTML = "";
+  for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = questions[currentQuestion].choices[i];
+    choices.appendChild(li);
+  }
+}
+
+updateQuestion();
+
+// Function to check if the answer is correct
+function checkAnswer(event) {
+  if (event.target.textContent === questions[currentQuestion].correctAnswer) {
+    score++;
+  }
+  currentQuestion++;
+  if (currentQuestion < questions.length) {
+    updateQuestion();
+  } else {
+    choices.innerHTML = "";
+    questionEl.textContent = `Your score is ${score}`;
+  }
+}
+
+choices.addEventListener("click", checkAnswer);

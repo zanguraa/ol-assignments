176b1a4e3d7c3a8dabbd6c30591315ecbafa4219
Zangura <zanguraa@gmail.com>
2024-01-21 13:44:03 +0400

    added RandomGroup and Random Member project

---
+++ GroupAndProjectRandomizer/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace GroupAndProjectRandomizer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    enum Project
+    {
+        BankingSystem,
+        EmployeeBonusManagement,
+        BudgetingAndExpenseTracker,
+        FeatureVotingSystem
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            List<string> people = new List<string>
+        {
+            "Avtandil Achelashvili",
+            "Gaga Demetrashvili",
+            "Giorgi Mshvildadze",
+            "Dachi Lekborashvili",
+            "Valeri Zanguri",
+            "Zura Iakobashvili",
+            "Irakli Svanidze",
+            "Luka Karzhalovi",
+            "Luka Makharadze",
+            "Mate Tsaava",
+            "Mikheil Gelenidze",
+            "Nino Dautashvili",
+            "Shota Medzvelia",
+            "Shota Khizanishvili",
+            "Jaba Chkheidze",
+            "Sopio Papiashvili"
+        };
+
+            Random random = new Random();
+            people = people.OrderBy(x => random.Next()).ToList();
+
+            List<List<string>> groups = new List<List<string>>();
+            for (int i = 0; i < people.Count; i += 2)
+            {
+                List<string> group = people.Skip(i).Take(2).ToList();
+                groups.Add(group);
+            }
+
+            DisplayGroups(groups);
+
+        }
+
+        static void DisplayGroups(List<List<string>> groups)
+        {
+            for (int i = 0; i < groups.Count; i++)
+            {
+                Console.WriteLine($"Group {i + 1}:");
+                foreach (var person in groups[i])
+                {
+                    Console.WriteLine(person);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+}

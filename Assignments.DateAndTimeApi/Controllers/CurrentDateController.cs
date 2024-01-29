4d329b6097b359347b9d60dfe54a0a84ce10916a
Zangura <zanguraa@gmail.com>
2024-01-29 23:58:16 +0400

    added DateTimeApi project with GET method CurrentDateTime

---
+++ Assignments.DateAndTimeApi/Controllers/CurrentDateController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignments.DateAndTimeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrentDateController : ControllerBase
+    {
+        [HttpGet("/CurrentDateTime")]
+
+        public string GetDate()
+        {
+            var date =  DateTime.Now;
+            string formattedDate = date.ToString("yyyy-MM-dd HH:mm");
+
+            return formattedDate;
+        }
+
+    }
+}

487e287093925c094244d754be890069248ae9fc
Zangura <zanguraa@gmail.com>
2024-01-29 17:32:59 +0400

    added FileReading method

---
+++ Assignments.FootballDataReader/Match.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments.FootballDataReader
+{
+    public class Match
+    {
+        public DateTime Date { get; set; }
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+        public string Tournament { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public bool Neutral { get; set; }
+
+        public Match(DateTime date, string homeTeam, string awayTeam, int homeScore, int awayScore, string tournament, string city, string country, bool neutral )
+        {
+            Date = date;
+            HomeTeam = homeTeam;
+            AwayTeam = awayTeam;
+            HomeScore = homeScore;
+            AwayScore = awayScore;
+            Tournament = tournament;
+            City = city;
+            Country = country;
+            Neutral = neutral;
+        }
+
+         public override string ToString()
+    {
+        return $"{Date.ToShortDateString()} - {HomeTeam} vs {AwayTeam}, Score: {HomeScore}-{AwayScore}, Tournament: {Tournament}, Location: {City}, {Country}, Neutral: {Neutral}";
+    }
+
+    }
+}
---
+++ Assignments.FootballDataReader/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Assignments.FootballDataReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string executablePath = AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(executablePath, "football_results.csv");
+            var data = FootballData(filePath);
+
+            foreach (var item in data)
+            {
+                Console.WriteLine(item);
+            }
+          
+
+        }
+
+        public static List<Match> FootballData(string filePath)
+        {
+            List<string> footballData = File.ReadAllLines(filePath).ToList();
+            footballData.RemoveAt(0);
+
+            List<Match> footballObject = new List<Match>();
+
+            foreach (string football in footballData)
+            {
+                string[] values = football.Split(',');
+
+                if (values[3] != "NA" && values[4] != "NA" &&
+                    int.TryParse(values[3], out int homeScore) && int.TryParse(values[4], out int awayScore))
+                {
+                    Match match = new Match(
+                        date: DateTime.Parse(values[0]),
+                        homeTeam: values[1],
+                        awayTeam: values[2],
+                        homeScore: homeScore,
+                        awayScore: awayScore,
+                        tournament: values[5],
+                        city: values[6],
+                        country: values[7],
+                        neutral: bool.Parse(values[8]));
+
+                    footballObject.Add(match);
+                }
+
+                else
+                {
+                    Console.WriteLine("Error: Invalid score values in a row");
+                }
+
+            }
+
+            return footballObject;
+        }
+
+
+
+
+    }
+}

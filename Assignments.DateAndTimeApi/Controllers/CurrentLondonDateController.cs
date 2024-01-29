64998a1d197fcfe74e49b72c14b460c58d1470e0
Zangura <zanguraa@gmail.com>
2024-01-30 00:25:45 +0400

    added LondonCurrentTime controller

---
+++ Assignments.DateAndTimeApi/Controllers/CurrentLondonDateController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignments.DateAndTimeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrentLondonDateController
+    {
+        [HttpGet("/CurrentLondonDate")]
+
+        public string CurrentLondonDate()
+        {
+            var tbilisiTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Asia/Tbilisi");
+            var tbilisiTime = TimeZoneInfo.ConvertTime(DateTime.Now, tbilisiTimeZone);
+
+            var londonTimeZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time"); 
+            var londonTime = TimeZoneInfo.ConvertTime(tbilisiTime, londonTimeZone);
+
+            return londonTime.ToString();
+        }
+    }
+}

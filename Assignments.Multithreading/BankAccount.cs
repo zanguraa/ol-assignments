baf929347da975e3e5077d97295786434e24fc36
Zangura <zanguraa@gmail.com>
2024-02-04 16:20:45 +0400

    added Multithreading task

---
+++ Assignments.Multithreading/BankAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments.Multithreading;
+
+public class BankAccount
+{
+    public string Iban { get; set; } = "";
+    public decimal Balance { get; set; }
+}
---
+++ Assignments.Multithreading/TransactionService.cs
@@ -0,0 +1,20 @@
+﻿namespace Assignments.Multithreading;
+
+public class TransactionService
+{
+    public void Transfer(BankAccount fromAccount, BankAccount toAccount, decimal amount)
+    {
+        lock (this)
+        {
+            if (amount > fromAccount.Balance)
+            {
+                Console.WriteLine($"{fromAccount.Iban} has Insufficient funds");
+                return;
+            }
+
+            fromAccount.Balance -= amount;
+            toAccount.Balance += amount;
+        }
+       
+    }
+}

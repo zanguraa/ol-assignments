025902560879db0f774c1db5a7b46f7743314db4
Zangura <zanguraa@gmail.com>
2024-02-08 19:27:51 +0400

    added startup for project RssAggregator

---
+++ Assignments.RssFeedAggregator/Db/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments.RssFeedAggregator.Db
+{
+    public static class DataContext
+    {
+        public static SqlConnection GetConnection()
+        {
+            // Return your SQL Server connection string here
+            string connectionString = "Your_SQL_Server_Connection_String";
+            return new SqlConnection(connectionString);
+        }
+    }
+}
---
+++ Assignments.RssFeedAggregator/Models/RssItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments.RssFeedAggregator.Models
+{
+    class RssItem
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+    }
+}

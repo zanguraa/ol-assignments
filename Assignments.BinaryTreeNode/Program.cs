dd016ac9b641b839d6a0d563b19870a776f59494
Zangura <zanguraa@gmail.com>
2024-02-08 16:06:56 +0400

    refactored code

--- Assignments.BinaryTreeNode/Program.cs
+++ Assignments.BinaryTreeNode/Program.cs
@@ -2,6 +2,26 @@
 {
     public class Program
     {
+        public static void Main()
+        {
+            var root = new BinaryTreeNode(1);
+
+            root.Left = new BinaryTreeNode(2);
+            root.Right = new BinaryTreeNode(3);
+
+            root.Left.Left = new BinaryTreeNode(4);
+            root.Left.Right = new BinaryTreeNode(5);
+            root.Right.Left = new BinaryTreeNode(6);
+            root.Right.Right = new BinaryTreeNode(7);
+
+            root.Left.Left.Left = new BinaryTreeNode(8);
+            root.Left.Left.Right = new BinaryTreeNode(9);
+
+            root.Left.Right.Left = new BinaryTreeNode(10);
+
+            var sums = BranchSums(root);
+            Console.WriteLine(string.Join(", ", sums));
+        }
         private static void CalculateBranchSums(BinaryTreeNode node, int runningSum, List<int> sums)
         {
             if (node == null)
@@ -24,27 +44,6 @@
             List<int> sums = new List<int>();
             CalculateBranchSums(root, 0, sums);
             return sums;
-        }
-
-        public static void Main()
-        {
-            var root = new BinaryTreeNode(1);
-
-            root.Left = new BinaryTreeNode(2);
-            root.Right = new BinaryTreeNode(3);
-
-            root.Left.Left = new BinaryTreeNode(4);
-            root.Left.Right = new BinaryTreeNode(5);
-            root.Right.Left = new BinaryTreeNode(6);
-            root.Right.Right = new BinaryTreeNode(7);
-
-            root.Left.Left.Left = new BinaryTreeNode(8);
-            root.Left.Left.Right = new BinaryTreeNode(9);
-
-            root.Left.Right.Left = new BinaryTreeNode(10);
-
-            var sums = BranchSums(root);
-            Console.WriteLine(string.Join(", ", sums)); 
         }
     }
 }
